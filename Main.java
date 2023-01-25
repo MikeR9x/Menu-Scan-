a23530d907bc7b3eeba7983115a995c3f91164f4
MikeR9x <122308105+MikeR9x@users.noreply.github.com>
2023-01-25 12:29:16 -0600

    Add files via upload

--- Main.java
+++ Main.java
@@ -3,14 +3,11 @@ import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
-		
-		
-		
 		try (Scanner scan = new Scanner(System.in)) {
 			System.out.println("Ingresa la figura que quieres hacer: circulo o cuadrado");
-			String figura = scan.nextLine();
+			int contador = 0;
+			while(contador==0) {
+				String figura = scan.nextLine();
 			if(figura.contains("cuadrado")) {
 				Cuadrado c = new Cuadrado();
 				System.out.println("Ingresa la longitud del Cuadrado");
@@ -18,12 +15,13 @@ public class Main {
 				System.out.println("La longitud es: " + c.longitud);
 				System.out.println("Ingrese la operación que desea calcular: diagonal, area o perimetro");
 				figura = scan.next();
+				contador++;
 				if(figura.contains("diagonal")) {
-					System.out.println(c.calcularDiagonal());
+					System.out.println("La diagonar el: " + c.calcularDiagonal());
 				}else if(figura.contains("area")) {
-					System.out.println(c.calcularArea());
+					System.out.println("El area es: " + c.calcularArea());
 				}else if(figura.contains("perimetro")) {
-					System.out.println(c.calcularPerimetro());
+					System.out.println("El perimetro es: " + c.calcularPerimetro());
 				}
 				
 			}else if(figura.contains("circulo")) {
@@ -33,13 +31,18 @@ public class Main {
 				System.out.println("El radio es: " + z.radio);
 				System.out.println("Que desea calcular: circunferencia o area");
 				figura = scan.next();
+				contador++;
 				if(figura.contains("circunferencia")) {
 					System.out.println("La circunferencia es: " + z.calcularCircunferencia());
 				} else if (figura.contains("area")) {
-					System.out.println(z.calcularArea());
+					System.out.println("El area es: " + z.calcularArea());
 				}
 			}
-			
+			if(contador==1) {
+				break;
+			}
+			System.out.println("Error. Escriba la figura nuevamente");
+		}
 		}
 		
 	}
